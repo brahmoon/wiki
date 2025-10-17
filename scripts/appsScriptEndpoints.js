189181d7b03cadd995a5060051d96ebedf88cbad
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-10-17 14:36:13 +0900

    Update appsScriptEndpoints.js

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -8,7 +8,7 @@
     DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbyvK1s6Zs4ccn_yeSh2Xz8MWOa5AO8y_GsqRr8BacITxMU8MhcEFDVm7IT56ZZUvWXE/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbw4DPAmRq9f-zikBUH4MLWxfhVPmMI1dJpmMWeiDWkKxQwPKLPZa1wp4pZk3eav2zJ4/exec',
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxqJEfpYVc3Xtpf_NXdB0j-EUDefyFw04F1ZEtPnandolXMUsxnSAlQUVk8FL5hz0BubA/exec',
-    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbx3fjjyy7ioBSrhQC07zZ4RFdUMVVsWD8OuZvq0kcCwXP7k1FX1_X4L9_MRbAM4OgYmpA/exec',
+    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbzLYkmWzTM7-uu1HeYK7yf8YmOF3hKTr6ddun8Z266SnexD0cUQqs2SORxniltZKUFcQQ/exec',
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxqJEfpYVc3Xtpf_NXdB0j-EUDefyFw04F1ZEtPnandolXMUsxnSAlQUVk8FL5hz0BubA/exec',
   });
 
