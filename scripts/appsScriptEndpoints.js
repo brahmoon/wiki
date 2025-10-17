b4b2fb73cade430b36382584b0f9275ff5191ab6
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-10-17 17:45:09 +0900

    Update appsScriptEndpoints.js

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -5,9 +5,9 @@
 
   const endpoints = Object.freeze({
     WIKI: 'https://script.google.com/macros/s/AKfycbyFiFm2zdtkPhuZ4EOJ-yLEGnw_opM5hxQBb7EE-sSg2mJ9pOU3HTrFoAXgIeb5J2Q91w/exec',
-    DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxpSrRROVPTfh90XESu1yZswGNkzJoM0jIfrmU-pkdrJJs3G_crSPR-lWjyrTnedpcL/exec',
+    DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxsOENqgLhQGUjNPQkVisW9ygusaT0shl2tUIAsvNIBSY5eOgFD4OPjh4JpZ4t2-XEg/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbw4DPAmRq9f-zikBUH4MLWxfhVPmMI1dJpmMWeiDWkKxQwPKLPZa1wp4pZk3eav2zJ4/exec',
-    ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxqJEfpYVc3Xtpf_NXdB0j-EUDefyFw04F1ZEtPnandolXMUsxnSAlQUVk8FL5hz0BubA/exec',
+    ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbzcOdiJgauxnmZi3YQVnRY2iYUSb5MYDaOwpZJgcjTc1GkvsckfPCd6lvYP5ElrqFFQRQ/exec',
     USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbzLYkmWzTM7-uu1HeYK7yf8YmOF3hKTr6ddun8Z266SnexD0cUQqs2SORxniltZKUFcQQ/exec',
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxqJEfpYVc3Xtpf_NXdB0j-EUDefyFw04F1ZEtPnandolXMUsxnSAlQUVk8FL5hz0BubA/exec',
   });
