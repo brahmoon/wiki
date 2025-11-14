71035821cd578f8b2f7169732c856c3c35c7dedf
brahmoon <3rd.reaper@gmail.com>
2025-11-14 19:50:03 +0900

    change api

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -4,7 +4,7 @@
   }
 
   const endpoints = Object.freeze({
-    WIKI: 'https://script.google.com/macros/s/AKfycbwKI1E2GHOHvzp6SGOEznlId_QF_SEBuTMfUoszKEmFcTeCTzjkQQwogAyGIx-inGK49Q/exec',
+    WIKI: 'https://script.google.com/macros/s/AKfycbwVzZX57xLz-p-DW15XUASzSeiZcoXugSCZzFSae_Y7vAxljtF2TTmU2DowulsZI65LHg/exec',
     DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxaS2LMlxk5rx-XK6XTkFG2S58NKTkOnBL3smjF_mbyP_QA0PDJJ2hBE0_-PEX7nSO4/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbwHhnYgB02en5yytMZEp37uj6R6sG9avJaI-NiBPtlNOm1hf5KkICOeMqPMrIIIne8k/exec',
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
