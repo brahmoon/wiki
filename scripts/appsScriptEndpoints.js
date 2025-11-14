69dc475979b0e7912c79c67f18092cc524ba7c26
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-11-14 17:01:42 +0900

    change api

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -7,9 +7,9 @@
     WIKI: 'https://script.google.com/macros/s/AKfycbyFiFm2zdtkPhuZ4EOJ-yLEGnw_opM5hxQBb7EE-sSg2mJ9pOU3HTrFoAXgIeb5J2Q91w/exec',
     DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxaS2LMlxk5rx-XK6XTkFG2S58NKTkOnBL3smjF_mbyP_QA0PDJJ2hBE0_-PEX7nSO4/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbwHhnYgB02en5yytMZEp37uj6R6sG9avJaI-NiBPtlNOm1hf5KkICOeMqPMrIIIne8k/exec',
-    ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbzy0MecmvBl8TWzPmnAanAnRiYQPbclcPSa5oSuX1ViJ96rMzuk73mA1uKQyOasBxd02w/exec',
+    ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
     USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbxLDRipxZs2bO2PDxD5RNlIZTlWKsHIhqyIuUAcF4uS0HImaUjmiB3qYrON0_hF4AbtLQ/exec',
-    TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbzy0MecmvBl8TWzPmnAanAnRiYQPbclcPSa5oSuX1ViJ96rMzuk73mA1uKQyOasBxd02w/exec',
+    TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
   });
 
   globalObject.APPS_SCRIPT_ENDPOINTS = endpoints;
