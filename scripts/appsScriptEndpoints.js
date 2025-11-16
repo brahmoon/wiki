abc620b3ef3c50431dba190c0b83c758483b5e20
brahmoon <3rd.reaper@gmail.com>
2025-11-16 18:34:14 +0900

    api change

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -8,7 +8,7 @@
     DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxaS2LMlxk5rx-XK6XTkFG2S58NKTkOnBL3smjF_mbyP_QA0PDJJ2hBE0_-PEX7nSO4/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbwHhnYgB02en5yytMZEp37uj6R6sG9avJaI-NiBPtlNOm1hf5KkICOeMqPMrIIIne8k/exec',
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
-    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbwu9vYcTebidTbpBGLJbQzMJgu2XBByoSMfcVSog_a1Tmkmy7e6qk0Hye4jXPlUgUdb4g/exec',
+    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbzY9aHtc-xCusIOE_2PzNCL3K9f6sQKbIU-xjTLyP_ez8YqsgAMAhpAPxhnl_CySAeJIw/exec',
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
   });
 
