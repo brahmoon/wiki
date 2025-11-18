81eb12b7901001808322410094518764d35b62c8
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-11-18 19:41:44 +0900

    Fix parseRequestData in API

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -8,7 +8,7 @@
     DRIVE_IMAGE: 'https://script.google.com/macros/s/AKfycbxaS2LMlxk5rx-XK6XTkFG2S58NKTkOnBL3smjF_mbyP_QA0PDJJ2hBE0_-PEX7nSO4/exec',
     LOGIN: 'https://script.google.com/macros/s/AKfycbwHhnYgB02en5yytMZEp37uj6R6sG9avJaI-NiBPtlNOm1hf5KkICOeMqPMrIIIne8k/exec',
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
-    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbzmSre34EpwxZoK14dNztkwLYZVghu9oOsclm0Rbu5VrzDvNbRZQoIF-_bx8CQsLOAnbg/exec',
+    USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbyStV61lAf6FMHDZFufLsgCy8hl1Pfc0yWQxOhB0AKH4qjvcHPhm2ZwTHdrzkw4AsL6dg/exec',
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxY_eqIlG2elhiNqtnBi__jUywu7LuanURjT0HA4uI2cOdnLbD9cCEiDGaqFEdz0-BHEA/exec',
   });
 
