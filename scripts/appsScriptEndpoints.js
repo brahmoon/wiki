fee9891ef2eaee1bd261bfcc5c02955b3a4a9787
brahmoon <3rd.reaper@gmail.com>
2025-11-23 02:50:20 +0900

    update api

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -10,7 +10,7 @@
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxEKtDO85fhESn4fyk9aQqCqPC2rBvFqvSG0Kdz8nLUfq_b68WyLmpenuv2R_kG6BL5SA/exec', // gs/authorize.gs
     USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbxcXErh3Q-F8ARTpznVQ7hgyLu5-IxWefS_63K_K8AwKmDGcqLa3Fiip44OeF1cMWKGRw/exec', // gs/user-setting.gs
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxEKtDO85fhESn4fyk9aQqCqPC2rBvFqvSG0Kdz8nLUfq_b68WyLmpenuv2R_kG6BL5SA/exec', // gs/authorize.gs (toolbar plugin API)
-    DATABASE: 'https://script.google.com/macros/s/AKfycbxL6cEzKR07xGbt7pVWx90vAfvmy8Qp42G8YrQ3x2I_RIZpTBn-10VJZfXjgH07V142ew/exec', // gs/database.gs
+    DATABASE: 'https://script.google.com/macros/s/AKfycbzxA2RPx0-9hxsXNlf3NswSLCsjDgnrXKAoXFbb5ILJ-7WVZO6t0fQw5EH_QZUBtGyJ8g/exec', // gs/database.gs
   });
 
   globalObject.APPS_SCRIPT_ENDPOINTS = endpoints;
