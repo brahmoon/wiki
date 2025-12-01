1968ac569eeb14c3ecd7d2ddc72af69ff22427fb
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-12-01 13:31:46 +0900

    api fix

--- scripts/appsScriptEndpoints.js
+++ scripts/appsScriptEndpoints.js
@@ -10,7 +10,7 @@
     ADMIN_AUTH: 'https://script.google.com/macros/s/AKfycbxEKtDO85fhESn4fyk9aQqCqPC2rBvFqvSG0Kdz8nLUfq_b68WyLmpenuv2R_kG6BL5SA/exec', // gs/authorize.gs
     USER_SETTINGS: 'https://script.google.com/macros/s/AKfycbxcXErh3Q-F8ARTpznVQ7hgyLu5-IxWefS_63K_K8AwKmDGcqLa3Fiip44OeF1cMWKGRw/exec', // gs/user-setting.gs
     TOOLBAR_PLUGIN: 'https://script.google.com/macros/s/AKfycbxEKtDO85fhESn4fyk9aQqCqPC2rBvFqvSG0Kdz8nLUfq_b68WyLmpenuv2R_kG6BL5SA/exec', // gs/authorize.gs (toolbar plugin API)
-    DATABASE: 'https://script.google.com/macros/s/AKfycbzxA2RPx0-9hxsXNlf3NswSLCsjDgnrXKAoXFbb5ILJ-7WVZO6t0fQw5EH_QZUBtGyJ8g/exec', // gs/database.gs
+    DATABASE: 'https://script.google.com/macros/s/AKfycbx41yPJxQdH9kP136rmTk9yYn3Ua0CNMKS06Lr85th8WbR_wgCqi6vR4EFI1Qf5BlzAmQ/exec', // gs/database.gs
   });
 
   globalObject.APPS_SCRIPT_ENDPOINTS = endpoints;
